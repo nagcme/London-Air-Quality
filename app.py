95ec506aca84dbe54ede28462e824d4baeb74f87
nagcme <72577997+nagcme@users.noreply.github.com>
2021-09-01 11:57:54 +0530

    Update comments

--- app.py
+++ app.py
@@ -20,27 +20,28 @@ import json
 from flask import Flask, render_template, request
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
-# from itertools import repeat
 
 # creates the WSGI application
 app = Flask(__name__)
 
-
+# This function receives the borough name as input. It then fetches the date
+# when each air pollutant had the least emission and outputs a dataframe
+# containing the air pollutant name, date and the corresponding minimum value of air
+# pollutant
 def fetch_min_air_pollutant(borough_name):
-    df_data = pd.read_csv('data_mean/data_borough/Daily_{}.csv'.format(
+    air_data = pd.read_csv('data_mean/data_borough/Daily_{}.csv'.format(
               borough_name))
-    df_data['date'] = df_data.MeasurementDateGMT
-    df_data.index = pd.to_datetime(df_data.date)
-    df_data = df_data.drop(['MeasurementDateGMT'], axis=1)
+    air_data['date'] = air_data.MeasurementDateGMT
+    air_data.index = pd.to_datetime(air_data.date)
+    air_data = air_data.drop(['MeasurementDateGMT'], axis=1)
 
     col_list = ['AirPollutant', 'Date', 'Value']
     df_min_air = pd.DataFrame(columns=col_list)
 
-    for col in df_data:
+    for col in air_data:
         if col == 'date':
             continue
-        # data = df_data[df_data[col] == df_data[col].min()]
-        data = df_data.loc[[df_data.loc[df_data[col] > 0, col].idxmin()]]
+        data = air_data.loc[[air_data.loc[air_data[col] > 0, col].idxmin()]]
         air_date = pd.to_datetime(data.date).dt.date
         val = data[col].values[0]
         val = round(val, 2)
@@ -49,15 +50,18 @@ def fetch_min_air_pollutant(borough_name):
         df_min_air = df_min_air.append(df)
     return df_min_air.to_html(index=False)
 
+# This function receives the borough name as input and outputs a multi-line
+# chart to showcase the weekly trent of each air pollutant in a plotly
+# interactive chart
 def plot_line_air_pollutant(borough_name):
-    df_data = pd.read_csv('data_mean/data_borough/{}.csv'.format(borough_name))
-    df_data['date'] = df_data.MeasurementDateGMT
-    df_data = df_data.drop(['MeasurementDateGMT'], axis=1)
+    air_data = pd.read_csv('data_mean/data_borough/{}.csv'.format(borough_name))
+    air_data['date'] = air_data.MeasurementDateGMT
+    air_data = air_data.drop(['MeasurementDateGMT'], axis=1)
     if borough_name == 'Wandsworth':
-        df_data['date'] = pd.to_datetime(df_data['date'], format='%d/%m/%Y')
-        df_data['date'] = df_data['date'].dt.strftime('%Y-%m-%d')
+        air_data['date'] = pd.to_datetime(air_data['date'], format='%d/%m/%Y')
+        air_data['date'] = air_data['date'].dt.strftime('%Y-%m-%d')
     # df_data['date'] = df_data.index
-    df_data_wo_date = df_data.drop(['date'], axis=1)
+    df_data_wo_date = air_data.drop(['date'], axis=1)
     max_range = df_data_wo_date.max().max()
 
     fig = go.Figure(
@@ -73,24 +77,21 @@ def plot_line_air_pollutant(borough_name):
             title="Air Pollutant Timeline",
         ))
 
-    # Add traces
+    # Add traces to the plot
     init = 1
-
-    color_list = ['#00ffbf', '#ffbf00', '#bfff00', '#bf00ff', '#00bfff',
-                  '#00ff80', '#ff8000', '#00ff80']
 
     color_dict = {'NO': '#EF9A9A', 'NO2': '#CE93D8', 'NOX': '#90CAF9',
                   'O3': '#A5D6A7', 'PM10': '#E6EE9C',
                   'PM25': '#BCAAA4', 'SO2': '#ff944d', 'CO': '#4d4d33'}
 
     i = 0
-    for col in df_data.columns:
+    for col in air_data.columns:
         if col == 'date':
             continue
         i = i + 1
         fig.add_trace(
-            go.Scatter(x=df_data.date[:init],
-                       y=df_data[col][:init],
+            go.Scatter(x=air_data.date[:init],
+                       y=air_data[col][:init],
                        name=col,
                        visible=True,
                        line=dict(color=color_dict.get(col))))
@@ -98,17 +99,17 @@ def plot_line_air_pollutant(borough_name):
     # Animation
     frame_list = []
 
-    for k in range(init, len(df_data) + 1):
+    for k in range(init, len(air_data) + 1):
 
         animation_list = []
-        for col in df_data.columns:
+        for col in air_data.columns:
             animation_list.append(
-                go.Scatter(x=df_data.date[:k], y=df_data[col][:k]))
+                go.Scatter(x=air_data.date[:k], y=air_data[col][:k]))
         frame_list.append(go.Frame(data=animation_list))
 
     fig.update(frames=frame_list)
 
-    # Extra Formatting
+    # Extra Formatting to update the lockdown period and ULEZ(where applicable)
     fig.update_xaxes(ticks="outside", tickwidth=2, tickcolor='white',
                      ticklen=10,mirror=True,showline=True)
     fig.update_yaxes(ticks="outside", tickwidth=2, tickcolor='white', ticklen=1,mirror=True,showline=True)
@@ -136,17 +137,16 @@ def plot_line_air_pollutant(borough_name):
                        text="Covid-19 Third Lockdown",
                        showarrow=False, textangle=-90)
 
-    # Buttons
-
+    # Create buttons for user interaction
     button_list = []
     button_list.append(dict(label="Play",
                             method="animate",
                             args=[None, {"frame": {"duration": 1000},"transition": {"duration": 0}}]))
 
-    visible_list = [False] * (len(df_data.columns) - 1)
+    visible_list = [False] * (len(air_data.columns) - 1)
     i = 0
 
-    for col in df_data.columns:
+    for col in air_data.columns:
         if col == 'date':
             continue
         visible_list[i] = True
@@ -158,7 +158,7 @@ def plot_line_air_pollutant(borough_name):
         visible_list[i] = False
         i = i + 1
 
-    visible_list = [True] * len(df_data.columns)
+    visible_list = [True] * len(air_data.columns)
 
     button_list.append(dict(label="All",
                             method="update",
@@ -171,15 +171,20 @@ def plot_line_air_pollutant(borough_name):
 
     fig.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 20
 
+    # Update axes for the line charts
     fig.update_xaxes(title_font_color='white', tickfont_color='white', gridcolor='grey')
     fig.update_yaxes(title_font_color='white', tickfont_color='white', gridcolor='grey')
     fig.update_layout(title_font_color='white', legend_font_color='white', paper_bgcolor='black', plot_bgcolor='black',title_font_size=25)
     fig.update_annotations(font_color='white',font_size=14)
 
+    # Return graph template in required format to render the graph object on
+    # HTML
     timegraph = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return timegraph
 
-
+# This function receives the borough name and a date (01-01-2018 -
+# 30-06-2021) and outputs the raw value of each pollutant in the borough on
+# that specific date in a gauge/speedometer chart
 def gauge_plot(borough_name,val_date='2021-06-30'):
 
     # Fetch data for input date and borough
@@ -219,10 +224,10 @@ def gauge_plot(borough_name,val_date='2021-06-30'):
 
     fig.update_layout(grid={'rows': 1, 'columns': i, 'pattern': "independent"})
     fig.update_layout(height=200,margin=dict(
-        l=10,
-        r=10,
-        b=10,
-        t=10,
+        l=9.8,
+        r=9.8,
+        b=9.8,
+        t=9.8,
         pad=4
     ))
     fig.update_layout(title_font_color='white', legend_font_color='white', paper_bgcolor='black', plot_bgcolor='black')
